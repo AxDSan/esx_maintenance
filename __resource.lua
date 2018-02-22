fe8aeb8ecb855614aa4f66d352c4a6dd56ba92d7
0x90 <abdi.moya@gmail.com>
2018-02-22 11:59:44 -0500

    Updated ESX Maintenance
    Removed a couple of things I wan't really using.

--- __resource.lua
+++ __resource.lua
@@ -3,11 +3,6 @@ description 'ESX Maintenance Mode'
 version '1.0'
 
 server_scripts {
-    '@mysql-async/lib/MySQL.lua',
     'server/main.lua',
     'cfg/config.lua'
-}
-
-client_scripts {
-    'client/main.lua'
 }
